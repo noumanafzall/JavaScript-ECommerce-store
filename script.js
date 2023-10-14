42ce154f0dcd3a14b4becd0a6bf0ca5279b7e72f
Nouman <noumanafzal.business@gmail.com>
2023-10-15 02:48:46 +0500

    Home Page

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
+const bar = document.getElementById('bar');
+const navBar = document.getElementById('navbar');
+const closeNavBar = document.getElementById('close');
 
 
+bar.addEventListener("click", () => {
+    navBar.classList.add("active");
+  });
+
+closeNavBar.addEventListener("click", () => {
+    navBar.classList.remove('active');
+})
