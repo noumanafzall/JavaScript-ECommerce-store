c7ddec60b876cd5cf730adbb45986f8ae6840932
Nouman <noumanafzal.business@gmail.com>
2023-10-21 14:59:54 +0500

    Structure complete

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// To Open and Close Nav bar
+
 const bar = document.getElementById('bar');
 const navBar = document.getElementById('navbar');
 const closeNavBar = document.getElementById('close');
@@ -10,3 +12,15 @@ bar.addEventListener("click", () => {
 closeNavBar.addEventListener("click", () => {
     navBar.classList.remove('active');
 })
+
+
+// To Add products to carts 
+
+let addToCart = document.querySelectorAll('.add-cart')
+
+for( i = 0; i < addToCart.length; i++)
+{
+  addToCart[i].addEventListener('click', () => {
+    console.log("added to cart");
+  })
+}
