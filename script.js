e36810d59adca164146cb6b5dadce467fb6f62a4
Nouman <noumanafzal.business@gmail.com>
2023-10-27 23:10:28 +0500

    :(

--- script.js
+++ script.js
@@ -13,9 +13,66 @@ closeNavBar.addEventListener("click", () => {
 })
 
 
-// To Add products to carts 
+
+
+
+// To Add Products to cart
+var addCartItems = document.querySelectorAll('.add-to-cart');
+
+for (var i = 0; i < addCartItems.length; i++) {
+    var button = addCartItems[i];
+    button.addEventListener('click', addCartItemsClicked);
+}
+
+function addCartItemsClicked(event) {
+    var button = event.target;
+    var prodDetails = button.parentElement.parentElement;
+    var price = prodDetails.querySelector('.item-price').innerText;
+    var title = prodDetails.querySelector('.item-title').innerText;
+    var imageSrc = prodDetails.querySelector('.item-image-src').src;
+    addRowToCart(title, price, imageSrc);
+}
+
+
+function addRowToCart(title, price, imageSrc) {
+  var cartItems = document.getElementsByClassName('cart-table-body');
+  var cartRow = document.createElement('tr');
+
+  var cartRowContent = 
+  `
+    <tr class="cart-row">
+      <td><img src="${imageSrc}" alt=""></td>
+      <td>${title}</td>
+      <td class="cart-price">${price}</td>
+      <td><input class="cart-quantity" type="number" value="1" ></td>
+      <td class="item-total">${price}</td>
+      <td><a class="remove-item"><i class="fa-solid fa-circle-xmark"></i></a></td>
+    </tr>
+  `;
+
+  cartRow.innerHTML = cartRowContent;
+
+
+
+  for (var i = 0; i < cartItems.length; i++) {
+    cartItems[i].appendChild(cartRow.cloneNode(true));
+  }
+
+
+  // cartItems.appendChild(cartRow)
+
+
+  console.log(cartItems)
+}
+
+
+
+
+
+
+
+// To Remove products from carts 
 var removeCartItems = document.getElementsByClassName('remove-item');
-console.log(removeCartItems)
 for( var i = 0; i < removeCartItems.length; i++){
   var button = removeCartItems[i]
   button.addEventListener('click', function(event) {
@@ -30,7 +87,7 @@ function updateCartTotal(){
   var total = 0
   for( var i = 0; i < cartRows.length; i++){
     var cartRow = cartRows[i]
-    var priceElement = document.getElementsByClassName('cart-price')[0];
+    var priceElement = document.getElementsByClassName('cart-price')[0]
     var quantityElement = document.getElementsByClassName('cart-quantity')[0];
     var price = parseFloat(priceElement.innerText.replace('$',''))
     var quantity = quantityElement.value
